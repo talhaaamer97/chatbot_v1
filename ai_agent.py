d5b1f2bd6bc3a1368b0d003d6f9c59c4e15758ec
Talha Aamer <talhaaamer97@outlook.com>
2025-03-25 19:38:08 +0500

    Bare bones working

---
+++ ai_agent.py
@@ -0,0 +1,45 @@
+# set API keys for Groq, Open AI and Tavil
+import os
+from dotenv import load_dotenv
+load_dotenv
+
+GROQ_API_KEY=os.environ.get("GROQ_API_KEY")
+TAVILY_API_KEY=os.environ.get("TAVILY_API_KEY")
+OPENAI_API_KEY=os.environ.get("OPENAI_API_KEY")
+# Phase 1 complete
+
+# set up LLMs and Tools
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+openai_llm = ChatOpenAI(model='gpt-4o-mini')
+groq_llm = ChatGroq(model='llama-3.3-70b-versatile')
+
+search_tool = TavilySearchResults(max_results=2)
+# Phase 2 complete
+
+# set up AI agent with search tool functionality
+from langgraph.prebuilt import create_react_agent
+# to get the response we want/filter i.e omit the meta data etc.
+from langchain_core.messages.ai import AIMessage
+system_prompt= 'Act as a smart and friendly AI chatbot'
+
+agent = create_react_agent(
+    model= groq_llm,
+    tools= [search_tool],
+    # what kind of a role the agent will have
+    state_modifier=system_prompt
+)
+
+# testing querries
+query= 'tell me about the trends in the crypto market'
+state={"messages": query}
+# it will not only have the answer returend but the api calls and other meta data
+response = agent.invoke(state)
+# to get the AI message only
+messages= response.get("messages")
+ai_messages = [message.content for message in messages if isinstance(message, AIMessage)]
+
+
+print(ai_messages[-1])
